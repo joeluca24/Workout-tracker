08648202e2070b3c2ecfbf7622bc9e1805299c8d
joeluca24 <65137591+joeluca24@users.noreply.github.com>
2020-10-02 19:28:59 -0400

    updated html-routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,3 +1,18 @@
+
+  
+const router = require("express").Router();
+const path = require("path");
+
+router.get("/exercise", (req, res) => {
+res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+    });
+
+module.exports = router;
+
 module.exports = (app) => {
     app.get("/",( req , res ) =>{
         res.send("This is inside html-routes")
@@ -5,3 +20,19 @@ module.exports = (app) => {
     });
 
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
