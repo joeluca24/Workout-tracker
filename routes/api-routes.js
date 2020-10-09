01d25df979f9bf53cb08982b9fd383652085ddf2
joeluca24 <65137591+joeluca24@users.noreply.github.com>
2020-10-08 20:28:42 -0400

    fixed workout.js overidden files

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,11 @@
 const db = require("../models");
+const router = require("express").Router();
+const path = require("path");
+
+
+
 module.exports = (app) => {
+
     app.get("/api/workouts",(req,res)=>{
         db.Workout.find({})
         .then(dbWorkout => {
@@ -8,6 +14,16 @@ module.exports = (app) => {
         .catch(err =>{
             res.status(401).json(err);
         })
+        // /
+router.get("/exercise", (req, res) => {
+res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+    });
+
+module.exports = router;
     })
 };
 
